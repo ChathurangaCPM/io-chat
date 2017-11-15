c892e93d236ea77df4ebdb80c4a8aef1da70b72b
Chathuranga Pradeep <chathuranga@emarketingeye.com>
2017-11-15 16:47:20 +0530

    improvment

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var path    =   require('path');
 var async   =   require('async');
 var socketio=   require('socket.io');
 var express =   require('express');
-var mysql   =   require('mysql');
+// var mysql   =   require('mysql');
 
 var router  = express();
 var server  = http.createServer(router);
@@ -20,15 +20,15 @@ router.use(express.static(path.resolve(__dirname, 'client')));
 var messages  = [];
 var sockets   = [];
 
-users         =	[];
-connections	  =	[];
+users         = [];
+connections   = [];
 
-var connectionsmsql = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database : 'samplenodedb'
-});
+// var connectionsmsql = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "",
+//     database : 'samplenodedb'
+// });
 
 router.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
@@ -44,19 +44,21 @@ router.get('/chat', function(req, res){
 
 io.on('connection', function (socket) {
     connections.push(socket);
-  	console.log('Connected : %s socket connected', connections.length)
-  	socket.on('disconnect', function(data){
-  		if (!socket.username) return;
-  		users.splice(users.indexOf(socket.username),1);
-  		updateUsernames();
-  		// Disconnection
-  		connections.splice(connections.indexOf(socket), 1);
-  		console.log('Disconnected : %s socket connected', connections.length)
-  	});
+    console.log('Connected : %s socket connected', connections.length)
+    socket.on('disconnect', function(data){
+        if (!socket.username) return;
+        users.splice(users.indexOf(socket.username),1);
+        updateUsernames();
+        // Disconnection
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Disconnected : %s socket connected', connections.length)
+    });
 
-  	socket.on('send message', function(data){
-  		io.sockets.emit('new message', {msg:data, user: socket.username, socket: socket.id});
-  	});
+    socket.on('send message', function(data){
+        console.log(data);
+        io.sockets.emit('new message', {msg:data, user: socket.username, socket: socket.id, profileimg:profileimg});
+    });
+    
     // Already loging user
     socket.on('setlog', function(data, callback){
       callback(true);
@@ -67,43 +69,67 @@ io.on('connection', function (socket) {
       users.push({socket: socket.id, username: registername, profileimg: registerprofile});
       updateUsernames({socket: socket.id, username: registername, profileimg: registerprofile});
     });
-  	// New user
-  	socket.on('new user', function(data, callback){
-  		callback(true);
-  		socket.username	=	data.username;
-        connectionsmsql.connect(function(error){
-            if (!!error) {
-                console.log('Db not connected');
-            }else{
-                console.log('Db connected successfully');
-                registername    = data.username;
-                registerpass    = data.userpass;
-                registeremail   = data.useremail;
-                profileimg      = data.profileimg;
-                socket_id       = socket.id;
-                var currentdate = new Date();
-                var datetime    = currentdate.getDate() + "/"
-                                + (currentdate.getMonth()+1)  + "/"
-                                + currentdate.getFullYear() + "@"
-                                + currentdate.getHours() + ":"
-                                + currentdate.getMinutes();
+    // New user
+    socket.on('new user', function(data, callback){
+        callback(true);
+        socket.username =   data.username;
 
-                var sql = "INSERT INTO users (user_name, profile_image, socket_id, registered, password,email) VALUES ?";
 
-                var values = [[registername,profileimg,socket_id,datetime,registerpass,registeremail]];
+        registername    = data.username;
+        registerpass    = data.userpass;
+        registeremail   = data.useremail;
+        profileimg      = data.profileimg;
+        socket_id       = socket.id;
+        var currentdate = new Date();
+        var datetime    = currentdate.getDate() + "/"
+                        + (currentdate.getMonth()+1)  + "/"
+                        + currentdate.getFullYear() + "@"
+                        + currentdate.getHours() + ":"
+                        + currentdate.getMinutes();
+                        
+        // console.log(profileimg);
+                        
+        users.push({socket: socket.id, username: registername, profileimg: profileimg});
+        // updateUsernames(socket.username);
+        updateUsernames({socket: socket.id, username: registername, profileimg: profileimg});
 
-                connectionsmsql.query(sql, [values], function (err, result) {
+        
+        // connectionsmsql.connect(function(error){
+        //     if (!!error) {
+        //         console.log('Db not connected');
+        //     }else{
+        //         console.log('Db connected successfully');
+        //         // registername    = data.username;
+        //         // registerpass    = data.userpass;
+        //         // registeremail   = data.useremail;
+        //         // profileimg      = data.profileimg;
+        //         // socket_id       = socket.id;
+        //         // var currentdate = new Date();
+        //         // var datetime    = currentdate.getDate() + "/"
+        //         //                 + (currentdate.getMonth()+1)  + "/"
+        //         //                 + currentdate.getFullYear() + "@"
+        //         //                 + currentdate.getHours() + ":"
+        //         //                 + currentdate.getMinutes();
 
-                if (err) throw err;
-                    console.log("Number of records inserted: " + result.affectedRows);
-              		users.push({socket: socket.id, username: socket.username, profileimg: profileimg});
-              		// updateUsernames(socket.username);
-              		updateUsernames({socket: socket.id, username: socket.username, profileimg: profileimg});
-                });
-            }
-        });
+        //         // users.push({socket: socket.id, username: socket.username, profileimg: profileimg});
+        //         // // updateUsernames(socket.username);
+        //         // updateUsernames({socket: socket.id, username: socket.username, profileimg: profileimg});
+        //         // var sql = "INSERT INTO users (user_name, profile_image, socket_id, registered, password,email) VALUES ?";
+
+        //         // var values = [[registername,profileimg,socket_id,datetime,registerpass,registeremail]];
+
+        //         // connectionsmsql.query(sql, [values], function (err, result) {
+
+        //         // if (err) throw err;
+        //         //     console.log("Number of records inserted: " + result.affectedRows);
+        //              // users.push({socket: socket.id, username: socket.username, profileimg: profileimg});
+        //              // // updateUsernames(socket.username);
+        //              // updateUsernames({socket: socket.id, username: socket.username, profileimg: profileimg});
+        //         // });
+        //     }
+        // });
       // console.log(socket);
-  	});
+    });
 
     socket.on('is-typing', function(data){
        io.sockets.emit('is-typing-client', {socket: socket.id, username: data.username});
@@ -123,11 +149,11 @@ io.on('connection', function (socket) {
     //     }
     // })
 
-  	function updateUsernames(sinuser){
+    function updateUsernames(sinuser){
         console.log(users);
-  		io.sockets.emit('get users', users);
+        io.sockets.emit('get users', users);
         io.sockets.emit('get single user', sinuser);
-  	}
+    }
     // console.log(users);
   });
 
